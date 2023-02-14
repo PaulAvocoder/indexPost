a03abe1d47135da8fe22a413a927a7c34f12bbba
Paul <Paul@mail.ru>
2023-02-14 21:18:49 +0300

    init commit v2

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,24 @@
+package java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestClass {
+
+    @Test
+    public void testMethod1() {
+        String[] assertArray = {"1,3,4,5", "2", "3,4"};
+        String[] indexes = {"1,3-5", "2", "3-4"};
+        String[] resultAfterMethod1 = sequencesOfNumbers(indexes);
+        Assert.assertArrayEquals(assertArray, resultAfterMethod1);
+    }
+
+    @Test
+    public void testMethod2() {
+        int[][] assertArray = {{1, 2, 3}, {1, 2, 4}, {3, 2, 3}, {3, 2, 4}, {4, 2, 3}, {4, 2, 4}, {5, 2, 3}, {5, 2, 4}};
+        String[] indexes = {"1,3-5", "2", "3-4"};
+        int[][] resultAfterMethod2 = permutationsToArrInt(sequencesOfNumbers(indexes));
+        Assert.assertArrayEquals(assertArray, resultAfterMethod2);
+    }
+}
